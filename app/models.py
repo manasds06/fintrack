1406e06b27a7e2e51ec052c4d9a4a5252bad5561
Manas Shrestha <manasds06@gmail.com>
2025-10-13 18:39:36 -0400

    Created base table blueprint in model.py and transaction.py to validate data going in and out of API endpoints

---
+++ app/models.py
@@ -0,0 +1,13 @@
+from sqlalchemy import Column, Integer, String, Float, DateTime
+from datetime import datetime
+from .database import Base
+
+# TransactionTable class that tells sqlalchemy what type of table to create
+class TransactionTable(Base):
+    __tablename__ = "Transactions"
+
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String, index=True)
+    amount = Column(Float)
+    category = Column(String)
+    timestamp = Column(DateTime, default=datetime.utcnow)
---
+++ app/transactions.py
@@ -0,0 +1,17 @@
+from pydantic import BaseModel
+from datetime import datetime
+
+class TransactionBase(BaseModel):
+    amount: float
+    title: str
+    category: str = None
+
+class TransactionCreate(TransactionBase):
+    pass
+
+class Transaction(TransactionBase):
+    id: int
+    timestamp: datetime
+
+    class Config:
+        orm_mode = True
