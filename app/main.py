20aa8de488b7c868c3cb7252ea02eea103115ac5
Manas Shrestha <manasds06@gmail.com>
2025-10-14 00:19:01 -0400

    Bug Fixes

--- app/main.py
+++ app/main.py
@@ -6,7 +6,7 @@ from .validation_schemas import transactions
 from .core.database import engine, SessionLocal
 
 # Creates table for data
-models.Base.metadata.create_all(bind=engine)
+transaction_model.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
@@ -22,9 +22,9 @@ def root_point():
   return {"message": "API still working?"}
 
 # POST method at /transaction endpoint for user to create and add new transactions they made
-@app.post("/transactions/", response_model=transactions.Transaction)
-def create_transaction(transaction: transactions.TransactionCreate, db: Session = Depends(get_db)):
-    db_transaction = models.TransactionTable(**transaction.dict())
+@app.post("/transactions/", response_model=transaction_model.Transaction)
+def create_transaction(transaction: transaction_model.TransactionCreate, db: Session = Depends(get_db)):
+    db_transaction = transaction_model.TransactionTable(**transaction.dict())
     db.add(db_transaction)
     db.commit()
     db.refresh(db_transaction)
@@ -33,12 +33,12 @@ def create_transaction(transaction: transactions.TransactionCreate, db: Session 
 # GET method at /transaction endpoint for user to be able to see all transactions made by them
 @app.get("/transactions/", response_model=list[transactions.Transaction])
 def get_transactions(db: Session = Depends(get_db)):
-    return db.query(models.TransactionTable).all()
+    return db.query(transaction_model.TransactionTable).all()
 
 # GET method at /transaction endpoint for user to be able to see specific transactions made by them
 @app.get("/transactions/{transaction_id}", response_model=transactions.Transaction)
 def get_transaction(transaction_id: int, db: Session = Depends(get_db)):
-    transaction = db.query(models.TransactionTable).filter(models.TransactionTable.id == transaction_id).first()
+    transaction = db.query(transaction_model.TransactionTable).filter(transaction_model.TransactionTable.id == transaction_id).first()
     if not transaction:
         raise HTTPException(status_code=404, detail="Transaction not found")
     return transaction
@@ -46,7 +46,7 @@ def get_transaction(transaction_id: int, db: Session = Depends(get_db)):
 # PUT method at /transaction endpoint for user to be able to update transactions details
 @app.put("/transactions/{transaction_id}", response_model=transactions.Transaction)
 def update_transaction(transaction_id: int, updated_data: transactions.TransactionCreate, db: Session = Depends(get_db)):
-    transaction = db.query(models.TransactionTable).filter(models.TransactionTable.id == transaction_id).first()
+    transaction = db.query(transaction_model.TransactionTable).filter(transaction_model.TransactionTable.id == transaction_id).first()
     if not transaction:
         raise HTTPException(status_code=404, detail="Transaction not found")
 
@@ -60,7 +60,7 @@ def update_transaction(transaction_id: int, updated_data: transactions.Transacti
 # DELETE method at /transaction endpoint for user to be able to remove/delete specific transactions
 @app.delete("/transactions/{transaction_id}")
 def delete_transaction(transaction_id: int, db: Session = Depends(get_db)):
-    transaction = db.query(models.TransactionTable).filter(models.TransactionTable.id == transaction_id).first()
+    transaction = db.query(transaction_model.TransactionTable).filter(transaction_model.TransactionTable.id == transaction_id).first()
     if not transaction:
         raise HTTPException(status_code=404, detail="Transaction not found")
 
